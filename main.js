b37721b79e1149a8362ae6d3437966b206f3fad0
Camanucm <43412704+Camanucm@users.noreply.github.com>
2021-01-26 12:41:08 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const products = [
+    {id: 1, title: 'Notebook', price: 20000},
+    {id: 2, title: 'Mouse', price: 1500},
+    {id: 3, title: 'Keyboard', price: 5000},
+    {id: 4, title: 'Gamepad', price: 4500},
+];
+
+const renderProduct = (title, price, img = '') => {
+    return `<div class="product-item">
+                <h3>${title}</h3>
+                <p>${price}</p>
+                <button class="by-btn">Добавить в корзину</button>
+              </div>`;
+};
+
+// const renderProducts = (list) => {
+//     const productList = list.map(function (product) {
+//         return renderProduct(product.title, product.price);
+//     });
+//     console.log(productList);
+//     document.querySelector('.products').insertAdjacentHTML( 'beforebegin' , productList);
+//     // insertAdjacentHTML();
+// };
+
+renderProducts(products);
+
+function renderProducts (list) {
+    for (let product of list) {
+        document.querySelector('.products').insertAdjacentHTML( 'beforebegin' , renderProduct(product.title, product.price));
+    }
+}
